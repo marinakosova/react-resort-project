a0af1b2f8ac7768433125e7a6656165bf4be1e48
marinakosova <marina.kosova@gmail.com>
2021-02-16 13:09:19 -0600

    init: start resort project

---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+    render() {
+        return (
+            <div>
+                single room
+            </div>
+        );
+    }
+}
+
+export default SingleRoom;
