dabd2274380f66d17333513e76b6555eced73118
marinakosova <marina.kosova@gmail.com>
2021-02-18 13:29:22 -0600

    feat: add logo and nav icon

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import {
+  Switch,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
