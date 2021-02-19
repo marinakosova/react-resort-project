c898c65249d43074325ab7af4f6b6c797edf5240
marinakosova <marina.kosova@gmail.com>
2021-02-19 14:32:48 -0600

    feat: add room proptypes and images

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import { RoomContext } from '../context';
 import Loading from './Loading';
 import Room from './Room';
+import Title from './Title';
 
 class FeaturedRooms extends Component {
     static contextType = RoomContext
     render() {
-
+        let { loading, featuredRooms: rooms } = this.context;
+        rooms = rooms.map(room => {
+            return <Room key={room.id} room={room} />
+        })
         return (
-            <div>
-                featured
-                <Room />
-                <Loading />
-            </div>
+            <section className="featured-rooms">
+                <Title title="featured rooms" />
+                <div className="featured-rooms-center">
+                    {loading ? <Loading /> : rooms}
+                </div>
+            </section>
         );
     }
 }
