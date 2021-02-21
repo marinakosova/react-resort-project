b983024c595fabda48b9717c3ac70be9d9f4ea55
marinakosova <marina.kosova@gmail.com>
2021-02-21 14:43:05 -0600

    feat: add final filters

--- src/context.js
+++ src/context.js
@@ -50,7 +50,7 @@ class RoomProvider extends Component {
     handleChange = event => {
         const target = event.target;
         const name = event.target.name;
-        const value = event.type === 'checkbox' ? target.checked : target.value;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
         this.setState({
             [name]: value
         }, this.filterRooms)
@@ -71,6 +71,16 @@ class RoomProvider extends Component {
             tempRooms = tempRooms.filter(room => room.capacity >= capacity);
         }
         tempRooms = tempRooms.filter(room => room.price <= price);
+
+        tempRooms = tempRooms.filter(room => room.size >= minSize && room.size <= maxSize);
+
+        if (breakfast) {
+            tempRooms = tempRooms.filter(room => room.breakfast === true);
+        }
+
+        if (pets) {
+            tempRooms = tempRooms.filter(room => room.pets === true);
+        }
 
         this.setState({
             sortedRooms: tempRooms
